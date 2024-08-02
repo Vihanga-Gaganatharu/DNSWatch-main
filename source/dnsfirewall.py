26c889ed864b47c2d26195890ac175675e228106
Vihanga Gaganatharu <vihangagaganatharu1@gmail.com>
2024-08-02 13:47:48 +0530

    edit new

--- source/dnsfirewall.py
+++ source/dnsfirewall.py
@@ -5,8 +5,8 @@ from datetime import datetime, timedelta
 
 class DNSSpoofingDetector:
     def __init__(self, threshold=50, window_size=60):
-        self.threshold = threshold  # Zaman dilimi içindeki maksimum DNS sorgu sayısı
-        self.window_size = window_size  # Zaman dilimi (saniye cinsinden)
+        self.threshold = threshold  # Maximum number of DNS queries within the time window
+        self.window_size = window_size  # Time window (in seconds)
         self.dns_queries = defaultdict(list)
 
     def process_packet(self, pkt):
@@ -15,13 +15,13 @@ class DNSSpoofingDetector:
             src_ip = pkt[IP].src
             dns_id = pkt[DNS].id
 
-            # Önceki sorguları temizle
+            # Clean old queries
             self.clean_old_queries(query_time)
 
-            # Bu sorguyu listeye ekle
+            # Add this query to the list
             self.dns_queries[src_ip].append((dns_id, query_time))
 
-            # Belirli bir zaman dilimi boyunca sorgu sayısını kontrol et
+            # Check the number of queries within a specific time window
             if len(self.dns_queries[src_ip]) > self.threshold:
                 print(f"{Fore.RED}[!] Possible DNS spoofing detected from {src_ip}!")
                 print(f"{Fore.BLUE}\tNumber of DNS queries within the last {self.window_size} seconds: {len(self.dns_queries[src_ip])}")
@@ -30,10 +30,10 @@ class DNSSpoofingDetector:
                 print(Style.RESET_ALL)
 
     def clean_old_queries(self, current_time):
-        # Zaman dilimi dışındaki sorguları temizle
+        # Clean queries outside the time window
         for ip, queries in self.dns_queries.items():
             self.dns_queries[ip] = [(dns_id, query_time) for dns_id, query_time in queries
-                                     if query_time >= current_time - timedelta(seconds=self.window_size)]
+                                    if query_time >= current_time - timedelta(seconds=self.window_size)]
 
     def set_threshold(self, threshold):
         self.threshold = threshold
